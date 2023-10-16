013f5ab5d19abea2563b48bf8c001ccc6ee0b70d
kirdatatjana <kirda.tatjana@gmail.com>
2023-10-16 09:37:15 +0200

    User registration issue fixed

--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/AccountRegistrationDto.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/AccountRegistrationDto.cs
@@ -2,9 +2,10 @@ namespace Explorer.Stakeholders.API.Dtos;
 
 public class AccountRegistrationDto
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Email { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    
 }
