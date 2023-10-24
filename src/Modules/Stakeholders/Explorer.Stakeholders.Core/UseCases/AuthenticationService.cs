cb64213721da200effe6ea17f8803b7893030f5f
DeyoP <pelemisdejan6@gmail.com>
2023-10-24 13:34:49 +0200

    Tour preference creation after tourist registers

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/UseCases/AuthenticationService.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/UseCases/AuthenticationService.cs
@@ -13,13 +13,15 @@ public class AuthenticationService : IAuthenticationService
     private readonly IUserRepository _userRepository;
     private readonly ICrudRepository<Person> _personRepository;
     private readonly ICrudRepository<Profile> _profileRepository;
+    private readonly ITourPreferenceService _tourPreferenceService;
 
-    public AuthenticationService(IUserRepository userRepository, ICrudRepository<Person> personRepository, ITokenGenerator tokenGenerator, ICrudRepository<Profile> profileRepository)
+    public AuthenticationService(IUserRepository userRepository, ICrudRepository<Person> personRepository, ITokenGenerator tokenGenerator, ICrudRepository<Profile> profileRepository, ITourPreferenceService tourPreferenceService)
     {
         _tokenGenerator = tokenGenerator;
         _userRepository = userRepository;
         _personRepository = personRepository;
         _profileRepository = profileRepository;
+        _tourPreferenceService = tourPreferenceService;
     }
 
     public Result<AuthenticationTokensDto> Login(CredentialsDto credentials)
@@ -48,6 +50,21 @@ public class AuthenticationService : IAuthenticationService
             var user = _userRepository.Create(new User(account.Username, account.Password, UserRole.Tourist, true));
             //var person = _personRepository.Create(new Person(user.Id, account.Name, account.Surname, account.Email));
             var profile = _profileRepository.Create(new Profile(account.Name, account.Surname, account.ProfilePicture, account.Biography, account.Motto, user.Id, true));
+
+            var tourPreference = _tourPreferenceService.Create(
+                    new TourPreferenceDto
+                    {
+                        Id = (int)user.Id,
+                        TouristId = (int)user.Id,
+                        CarRating = 1,
+                        BoatRating = 1,
+                        WalkingRating = 1,
+                        BicycleRating = 1,
+                        Difficulty = 1,
+                        Tags = new List<string>()
+
+
+                    });
 
             return _tokenGenerator.GenerateAccessToken(user, profile.Id);
         }
