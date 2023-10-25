f700a8e27fb037cdaa5dd0e8828b6fe72e0319a9
Milos Diklic <milosdiklic25@gmail.com>
2023-10-25 21:19:25 +0200

    fix: minor fix

--- src/Explorer.API/Controllers/ClubController.cs
+++ src/Explorer.API/Controllers/ClubController.cs
@@ -1,6 +1,7 @@
 ﻿using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.Stakeholders.API.Dtos;
 using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Explorer.API.Controllers
