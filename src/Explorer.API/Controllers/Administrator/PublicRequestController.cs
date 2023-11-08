f75c3301be696800e5c993674308701d7389a5b7
Milos Diklic <milosdiklic25@gmail.com>
2023-11-09 00:35:38 +0100

    feat: public request controller added

---
+++ src/Explorer.API/Controllers/Administrator/PublicRequestController.cs
@@ -0,0 +1,31 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Explorer.Tours.Core.UseCases;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Administrator
+{
+    [Authorize(Policy = "administratorPolicy")]
+    [Route("api/administrator/publicRequest")]
+    [ApiController]
+    public class PublicRequestController : BaseApiController
+    {
+        private readonly IPublicRequestService _publicRequestService;
+
+        public PublicRequestController(IPublicRequestService publicRequestService)
+        {
+            _publicRequestService = publicRequestService;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "administrator")]
+        public ActionResult<PagedResult<TourDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _publicRequestService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+    }
+}
