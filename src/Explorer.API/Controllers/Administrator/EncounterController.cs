c02ddf95abfaf94bb91e82f66ce4bb7a71903775
Milos Diklic <milosdiklic25@gmail.com>
2023-11-25 23:50:52 +0100

    fix: renamed challenges into encounters

---
+++ src/Explorer.API/Controllers/Administrator/EncounterController.cs
@@ -0,0 +1,54 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Encounters.API.Dtos;
+using Explorer.Encounters.API.Public;
+using Explorer.Tours.API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Administrator
+{
+    [Route("api/administrator/encounter")]
+    [ApiController]
+    public class EncounterController : BaseApiController
+    {
+        private readonly IEncounterService _encounterService;
+        public EncounterController(IEncounterService encounterService)
+        {
+            _encounterService = encounterService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<EncounterDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _encounterService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<EncounterDto> Get(int id)
+        {
+            var result = _encounterService.Get(id);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<EncounterDto> Create([FromBody] EncounterDto challenge)
+        {
+            var result = _encounterService.Create(challenge);
+            return CreateResponse(result);
+        }
+
+        [HttpPut]
+        public ActionResult<EncounterDto> Update([FromBody] EncounterDto challenge)
+        {
+            var result = _encounterService.Update(challenge);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var result = _encounterService.Delete(id);
+            return CreateResponse(result);
+        }
+    }
+}
--- src/Modules/Encounters/Explorer.Encounters.Core/Domain/RepositoryInterfaces/IChallengeRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Explorer.Encounters.Core.Domain.RepositoryInterfaces
-{
-    public interface IChallengeRepository
-    {
-        Challenge Get(int id);
-        List<Challenge> GetAll();
-    }
-}
