ea059edee0bec56b99f24ba8da8cedc4f2285d96
vanja4 <valentinanikolic555@gmail.com>
2023-11-26 13:46:34 +0100

    Bundle model and test

---
+++ src/Explorer.API/Controllers/Tourist/BundleController.cs
@@ -0,0 +1,53 @@
+﻿using Explorer.Blog.API.Dtos;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Payments.API.Dtos;
+using Explorer.Payments.API.Public;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace Explorer.API.Controllers.Tourist
+{
+    [Route("api/tourist/bundle")]
+    public class BundleController : BaseApiController
+    {
+        private readonly IBundleService _bundleService;
+
+        public BundleController(IBundleService bundleService)
+        {
+            _bundleService = bundleService;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<BundleDto> Get(int id)
+        {
+            var result = _bundleService.Get(id);
+            return CreateResponse(result);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "author")]
+        public ActionResult<PagedResult<TourDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _bundleService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<BundleDto> Create([FromBody] BundleDto bundle)
+        {
+            var result = _bundleService.Create(bundle);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<BundleDto> Update([FromBody] BundleDto bundle)
+        {
+            var result = _bundleService.AddTour(bundle);
+            return CreateResponse(result);
+        }
+
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/BundleService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Explorer.Tours.Core.Domain;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.UseCases
+{
+    public class BundleService : CrudService<BundleDto, Bundle>, IBundleService
+    {
+        public BundleService(ICrudRepository<Bundle> crudRepository, IMapper mapper) : base(crudRepository, mapper)
+        {
+        }
+
+        public Result<BundleDto> Create(BundleDto bundleDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<BundleDto> AddTour(BundleDto bundleDto)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
