6c41950b87b032161704cc4d327b993d41e9ac49
tasha2812 <miljevictamara@gmail.com>
2023-11-26 11:03:06 +0100

    changes...

--- src/Modules/Payments/Explorer.Payments.Tests/TestData/b-wallet.sql
+++ src/Modules/Payments/Explorer.Payments.Tests/TestData/b-wallet.sql
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Explorer.Payments.Tests.TestData
-{
-    class b_wallet
-    {
-    }
-}
+﻿
