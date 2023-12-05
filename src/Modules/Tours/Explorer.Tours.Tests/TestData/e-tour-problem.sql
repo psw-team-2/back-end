150ae38ef993c5457319a94cb7e226d93861f564
Nina <nina.batranovic3@gmail.com>
2023-12-05 17:54:11 +0100

    sql fixed

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/e-tour-problem.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/e-tour-problem.sql
@@ -2,3 +2,7 @@
     "Id", "ProblemCategory", "ProblemPriority", "Description", "TimeStamp", "TourId", "IsClosed", "IsResolved", "TouristId", "DeadlineTimeStamp")
 VALUES 
 (-1, 'CATEGORY 1', 'PRIORITY 1', 'Too much noise!', '2021-09-09 15:22:53', 1, FALSE, FALSE, -1, NULL);
+INSERT INTO tours."TourProblems"(
+    "Id", "ProblemCategory", "ProblemPriority", "Description", "TimeStamp", "TourId", "IsClosed", "IsResolved", "TouristId", "DeadlineTimeStamp")
+VALUES 
+(-2, 'CATEGORY 2', 'PRIORITY 2', 'Too much noise!', '2021-09-09 15:22:53', 1, FALSE, FALSE, -1, NULL);
