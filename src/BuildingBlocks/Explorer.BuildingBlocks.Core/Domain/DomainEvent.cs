666077fa67d38077fa9c869bae35f5623655117f
ayadjo <andjela1108@gmail.com>
2023-12-17 13:54:14 +0100

    Added DomainEvent and EventSourcingAggregate classes

---
+++ src/BuildingBlocks/Explorer.BuildingBlocks.Core/Domain/DomainEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.BuildingBlocks.Core.Domain
+{
+    public abstract class DomainEvent
+    {
+        public Guid Id { get; private set; }
+
+        public DomainEvent(Guid aggregateId) { 
+            Id = aggregateId;
+        }
+        
+    }
+}
---
+++ src/BuildingBlocks/Explorer.BuildingBlocks.Core/Domain/EventSourcingAggregate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.BuildingBlocks.Core.Domain
+{
+    public abstract class EventSourcingAggregate : Entity
+    {
+        public List<DomainEvent> Changes { get; private set; }
+        public int Version { get; protected set; }
+
+        public EventSourcingAggregate() {
+            Changes = new List<DomainEvent>();
+        }
+
+        public abstract void Apply(DomainEvent changes);
+
+    }
+}
