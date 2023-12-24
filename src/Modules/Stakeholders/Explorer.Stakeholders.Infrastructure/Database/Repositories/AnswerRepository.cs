4645a9dca503a2ef7f35a7effc5185f752d1b8dd
Nina <nina.batranovic3@gmail.com>
2023-12-24 16:59:03 +0100

    initial repositories

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Repositories/AnswerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Explorer.Stakeholders.Core.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Infrastructure.Database.Repositories
+{
+    public class AnswerRepository : IAnswerRepository
+    {
+    }
+}
