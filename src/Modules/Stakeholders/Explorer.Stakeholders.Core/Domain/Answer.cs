46ad98d6261a8c193a3c3a80a2461a5ae5a91da9
Nina <nina.batranovic3@gmail.com>
2023-12-24 16:32:10 +0100

    initial domain classes

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Answer.cs
@@ -0,0 +1,34 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class Answer : Entity
+    {
+        public long TouristId { get; init; }
+        public long AdminId { get; init; }
+
+        public string Text { get; init; }
+        public AnswerCategory Category { get; init; }
+        public bool Visability { get; init; }
+
+        public Answer(long touristId, long adminId, string text, AnswerCategory category, bool visability)
+        {
+            TouristId = touristId;
+            AdminId = adminId;
+            Text = text;
+            Category = category;
+            Visability = visability;
+        }
+
+    }
+
+    public enum AnswerCategory
+    {
+        Payment, Tour, TechnicalSupport, Other
+    }
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Question.cs
@@ -0,0 +1,23 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class Question : Entity
+    {
+        public long TouristId { get; init; }
+        public long AdminId { get; init; }
+        public string Text { get; init; }
+
+        public Question(long touristId, long adminId, string text)
+        {
+            TouristId = touristId;
+            AdminId = adminId;
+            Text = text;
+        }
+    }
+}
