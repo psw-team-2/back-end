e9514a6507ab3292d36106469beb4e2717330ec1
sonja434 <sonjasekulic434@gmail.com>
2023-12-26 16:15:58 +0100

    feat: Event sourcing classes

---
+++ src/BuildingBlocks/Explorer.BuildingBlocks.Core/Domain/DomainEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.BuildingBlocks.Core.Domain
+{
+    public abstract class DomainEvent
+    {
+        public DomainEvent(Guid aggregateId)
+        {
+            Id = aggregateId;
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
