05f419da98affb8ff2d353635a4e38857da81f3a
milossdjuric <milossdjuric@yahoo.com>
2023-12-27 03:06:39 +0100

    quickfix

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/c1-profiles.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/c1-profiles.sql
@@ -1,3 +1,1 @@
-﻿INSERT INTO stakeholders."Profiles" ("FirstName", "LastName", "ProfilePicture", "Biography", "Motto", "UserId", "IsActive", "Follows", "TourPreference", "QuestionnaireDone")
-VALUES 
-  ('Mina', 'Lastname1', 'prof_image1', 'A brief biography of Mina.', 'Motto for Mina', 1, true, '{"followers": [], "following": []}', '{"Tags": [], "CarRating": 1, "BoatRating": 1, "Difficulty": 1, "BicycleRating": 1, "WalkingRating": 1}', true);
+﻿
